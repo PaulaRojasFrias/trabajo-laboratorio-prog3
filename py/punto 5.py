2fb381d1a3df2810b9cff58c61568dee4901648d
Valentino Marchetti <valentinomarchettti@gmail.com>
2023-10-11 16:35:18 -0300

    TP4

--- py/punto 5.py
+++ py/punto 5.py
@@ -1,3 +1,9 @@
+# Localiza el error en el siguiente bloque de código. Crea una excepción para evitar que el programa
+# se bloquee y además explica en un mensaje al usuario la causa y/o solución:
+# resultado = 15 + "20"
+
+
+
 try: 
     resultado = 15 + "20"
 except ValueError:
